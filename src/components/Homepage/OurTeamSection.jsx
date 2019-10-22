dcf6858c24df0adb479c3bfdea6b991f2647010d
hughp23 <hughpaul14@gmail.com>
2019-10-22 21:50:52 +0100

    inital setup of components and style sheets

---
+++ src/components/Homepage/OurTeamSection.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class OurTeamSection extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default OurTeamSection;
---
+++ src/components/Homepage/PhotosOfWorkSection.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PhotosOfWorkSection extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default PhotosOfWorkSection;
